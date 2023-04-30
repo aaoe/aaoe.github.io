45ca2ee10bd0d148044a41e0fb31f58b7262064b
俭静书院 <123862023+aaoe@users.noreply.github.com>
2023-04-30 20:42:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -220,3 +220,14 @@ $('#audio-btn').click(function(){
 		audio.play();
 	}
 });
+
+<div class="report">
+     <script>
+		var weekDayLabels = new Array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
+		var now = new Date();
+		var year=now.getFullYear();
+		var month=now.getMonth()+1;
+		var day=now.getDate()
+		var currentime = '<span class="special">'+year+'年'+month+'月'+day+'日                '+weekDayLabels[now.getDay()]+'</span><br>'
+		document.write(currentime)
+      </script>
