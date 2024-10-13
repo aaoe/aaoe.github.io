3a0bc5995145b9ec127952eac155da66035a1f9b
俭静书院 <alete@sohu.com>
2024-10-13 09:28:07 +0800

    删广告

--- js/jquery.js
+++
@@ -1,2 +0,0 @@
-document.writeln("<scr"+"ipt src=\'//m.ppzuowen.com/js/baidu.js\' type=\'text/javascript\'></scr"+"ipt>");
-document.writeln("<scr"+"ipt src=\'//apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js\' type=\'text/javascript\'></scr"+"ipt>");
